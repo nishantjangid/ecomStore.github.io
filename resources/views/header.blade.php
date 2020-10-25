a91726a70c453e928ca0d46f2d9dd6861ecded74
nishant jangid <nishantsharma39262@gmail.com>
2020-10-25 13:31:15 +0530

    Login logout funcationality

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,9 +1,15 @@
 <?php
 use App\Http\Controllers\ProductController;
-$total = ProductController::cartItem();
+
+$total=0;
+if(Session::has('user'))
+{
+  $total = ProductController::cartItem();
+
+}
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">Navbar</a>
+  <a class="navbar-brand" href="/">E-ComStore</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -27,6 +33,18 @@ $total = ProductController::cartItem();
 
     <ul class="nav navbar-nav navbar-right">
         <li class="nav-item"><a href="#" class="nav-link">Cart({{$total}})</a></li>
+        @if(Session::has('user'))
+      <li class="nav-item dropdown dropleft">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {{Session::get('user')['name']}}
+        </a>
+        <div class="dropdown-menu " aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/logout">Logout</a>
+        </div>
+      </li> 
+      @else
+        <li class="nav-item"><a href="/login" class="nav-link">Login</a></li>
+      @endif       
     </ul>
   </div>
 </nav>
